09260f0b2e9d8762d3fd2977a47b107c94548c5d
kaykobadyiasin <kaykobadyiasin@gmail.com>
2023-03-29 01:39:24 +0600

    product cart adeded with calculation

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({cart}) => {
+    // const cart = props; // option 1
+    // const {cart} = props; // option 2
+
+    console.log(cart);
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice + totalShipping + tax;
+
+    return (
+        <div className='cart'>
+            <h3>Shopping cart</h3>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+        </div>
+    );
+};
+
+export default Cart;
